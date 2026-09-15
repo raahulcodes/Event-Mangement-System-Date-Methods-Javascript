56e14e76436b0056c4b5e712f2fdb1daa665657f
Rahul Sharma <hubstackrealm@gmail.com>
2026-09-15 23:21:19 +0530

    Enhance Event class with date information method
    Added a method to retrieve detailed date information for the event.

--- app.js
+++ app.js
@@ -21,11 +21,32 @@ class Event
     }
 }
 
-// creating the essential date and event start time variables for passing on to class private fields
+// creating the essential date and event start time variables for passing on to class private fields and converting the format to localized string format
 let eventDate = new Date().toLocaleDateString();
 let setEventTime = new Date();
 setEventTime.setHours(20,30,0,0);
 let eventTime = setEventTime.toLocaleTimeString("en-US");
 
+// creating a day array
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday' , 'Friday', 'Saturday'];
+
 let event = new Event("Michael Jackson USA Tour", eventDate, eventTime);
 console.log(event.display());
+
+// retrieving date information
+Event.prototype.date_Info = function()
+{
+    setEventTime.setFullYear(2026, 11, 12);
+    return ` Year: ${setEventTime.getFullYear()}
+    Month: ${setEventTime.getMonth()}
+    Day: ${days[setEventTime.getDay()]}
+    Date: ${setEventTime.getDate()}
+    Hours: ${setEventTime.getHours()}
+    Minutes: ${setEventTime.getMinutes()}
+    Seconds: ${setEventTime.getSeconds()}
+    Time: ${setEventTime.getTime()}
+    `;
+}
+
+console.log(event.date_Info());
+
